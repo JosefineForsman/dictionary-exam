450cd167c529258a1dfb54e9140a79aacc556e2c
Josefine Forsman <josefineforsman1@gmail.com>
2023-09-19 22:39:27 +0200

    useReducer is implemented

---
+++ src/components/LikedWordContext/LikedWordContext.jsx
@@ -0,0 +1,40 @@
+import "./LikedWordContext.css";
+
+import { createContext } from "react";
+import { useReducer } from "react";
+
+export const LikedWordContext = createContext(null);
+
+// reducer to help me add and delete favorite words.
+function LikedWordContextProvider({ children }) {
+  const [likedWord, likedWordDispatcher] = useReducer(likedWordReducer, []);
+
+  return (
+    <LikedWordContext.Provider
+      value={{ word: likedWord, dispatch: likedWordDispatcher }}
+    >
+      {children}
+    </LikedWordContext.Provider>
+  );
+}
+let id = 1;
+const likedWordReducer = (likedWord, action) => {
+  console.log(likedWord, action);
+
+  switch (action.type) {
+    case "added": // add word
+      return [
+        ...likedWord,
+        {
+          id: id++, // different id for every word so we also can delete it later if we want.
+          word: action.payload,
+        },
+      ];
+    case "deleted": // delete word
+      return likedWord.filter((word) => word.id !== action.payload);
+
+    default:
+      return;
+  }
+};
+export default LikedWordContextProvider;
