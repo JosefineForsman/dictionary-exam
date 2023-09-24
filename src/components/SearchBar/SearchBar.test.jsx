66cb17ac2479dd2c974fdf96448d4bf46b1082d0
Josefine Forsman <josefineforsman1@gmail.com>
2023-09-24 19:31:10 +0200

    trying out different matchers

--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -1,19 +1,48 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SearchBar from "./SearchBar";
 
 describe(SearchBar, () => {
   it(" should render SearchBar", () => {
-    render(<SearchBar inputSearch={""} setInputSearch={vi.fn()} />);
+    render(<SearchBar setInputSearch={vi.fn()} />);
   });
-  it("should be able to accept input from user", async () => {
-    render(<SearchBar inputSearch={""} setInputSearch={vi.fn()} />);
-    const user = userEvent.setup(); // create user.
+  it("should update the input value", async () => {
+    render(<SearchBar setInputSearch={vi.fn()} />);
+    const user = userEvent.setup();
     const input = screen.getByRole("textbox");
 
     await user.type(input, "cat");
 
     expect(input).toHaveValue("cat");
+  });
+  it("should show an error message when trying to search with an empty input", async () => {
+    render(<SearchBar setInputSearch={vi.fn()} />);
+    const input = screen.getByRole("textbox");
+
+    await userEvent.type(input, "{enter}");
+
+    const errorMessage = screen.getByText(
+      "You are trying to search with an empty input. Please try again bud."
+    );
+    expect(errorMessage).toBeInTheDocument();
+  });
+  it("should clear input value when Enter key is pressed", async () => {
+    render(<SearchBar setInputSearch={vi.fn()} />);
+    const input = screen.getByRole("textbox");
+
+    await userEvent.type(input, "dog");
+    fireEvent.keyDown(input, { key: "Enter", code: "Enter" });
+
+    await waitFor(() => {
+      expect(input).toHaveTextContent("");
+    });
+  });
+  it("should be able to search and display results", async () => {
+    render(<SearchBar setInputSearch={vi.fn()} />);
+    const input = screen.getByRole("textbox");
+
+    fireEvent.change(input, { target: { value: "dog" } });
+    fireEvent.keyDown(input, { key: "Enter", code: "Enter" });
   });
 });
