5eb724c35192844c32905ece53e9a7444968fabb
Josefine Forsman <josefineforsman1@gmail.com>
2023-09-24 19:30:38 +0200

    display word testing

--- src/components/DisplayWord/DisplayWord.test.jsx
+++ src/components/DisplayWord/DisplayWord.test.jsx
@@ -1,23 +1,76 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeAll } from "vitest";
+import { render, screen, fireEvent, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import DisplayWord from "./DisplayWord";
-import mockWords from "../../test/mockWords.json";
 import DisplayMessage from "../DisplayMessage/DisplayMessage";
 import LikedWordContextProvider from "../LikedWordContext/LikedWordContext";
+import { server } from "../../test/mockServer";
+import mockWords from "../../test/mockWords.json";
 
-describe(DisplayWord, () => {
-  it("Should render the heading Hello", async () => {
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+const user = userEvent.setup();
+
+describe("DisplayWord", () => {
+  it("should display words when inputSearch is provided", () => {
     render(
       <LikedWordContextProvider>
-        {" "}
-        // rendering this because i'm using dispatch in the component.
-        <DisplayWord inputSearch={mockWords} dispatch={vi.fn()} />
+        <DisplayWord inputSearch={mockWords} />
+      </LikedWordContextProvider>
+    );
+    const wordElements = screen.getAllByRole("heading", { level: 1 });
+    expect(wordElements.length).toBe(mockWords.length);
+  });
+  it("should toggle star when star icon is clicked", () => {
+    const { container } = render(
+      <LikedWordContextProvider>
+        <DisplayWord inputSearch={mockWords} />
+      </LikedWordContextProvider>
+    );
+    const starIcon = container.querySelector(".star");
+    fireEvent.click(starIcon);
+    expect(starIcon).toHaveClass("filled");
+  });
+  it("should not display words when inputSearch is empty or invalid", () => {
+    render(
+      <LikedWordContextProvider>
+        <DisplayWord inputSearch={[]} />
       </LikedWordContextProvider>
     );
 
-    const h1 = screen.getByRole("heading", { level: 1 });
+    const wordElements = screen.queryAllByRole("heading", { level: 1 });
+    expect(wordElements.length).toBe(0);
+  });
+  it("should not display words when inputSearch is null or undefined", () => {
+    render(
+      <LikedWordContextProvider>
+        <DisplayWord inputSearch={null} />
+      </LikedWordContextProvider>
+    );
+    const wordElements = screen.queryAllByRole("heading", { level: 1 });
+    expect(wordElements.length).toBe(0);
+  });
+  it("should diplay all information about a word", () => {
+    const { container } = render(
+      <LikedWordContextProvider>
+        <DisplayWord inputSearch={mockWords} />
+      </LikedWordContextProvider>
+    );
+
+    const h1 = screen.getByRole("heading", { name: /hello/i });
+    const definitions = screen.getAllByRole("listitem");
+    const audioElement = screen.getAllByTestId("audio");
+    const exampleElements = container.querySelectorAll("p.example");
+    const seeMoreBtn = screen.getByRole("button", { name: /see more/i });
 
     expect(h1).toBeInTheDocument();
+    expect(screen.getByText("noun")).toBeInTheDocument();
+    expect(screen.getByText("verb")).toBeInTheDocument();
+    expect(screen.getByText("interjection")).toBeInTheDocument();
+    expect(definitions.length).toBe(5);
+    expect(audioElement).toHaveLength(2);
+    expect(exampleElements.length).toBe(3);
+    expect(seeMoreBtn).toBeInTheDocument();
+    screen.debug();
   });
 });
