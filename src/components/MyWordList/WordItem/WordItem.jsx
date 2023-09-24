06414acf6a5ad87e5ed65962fe149aa28c975034
Josefine Forsman <josefineforsman1@gmail.com>
2023-09-24 19:30:06 +0200

    added color to star when being clicked

--- src/components/MyWordList/WordItem/WordItem.jsx
+++ src/components/MyWordList/WordItem/WordItem.jsx
@@ -2,46 +2,54 @@ import "./WordItem.css";
 import { useState } from "react";
 import Meaning from "../Meaning/Meaning";
 import Phonetic from "../Phonetic/Phonetic";
+
 function WordItem({ wordItem, theme, handleDeleteClick }) {
   const [toggleInfo, setToggleInfo] = useState(false);
+  const isLiked = wordItem?.isLiked || false;
+
+  console.log(wordItem.word);
+
   return (
     <div className={`my-word-list ${theme}`}>
       <div className="list-item-container">
         <div
-          className={`remove-star ${wordItem.isLiked ? "" : "filled"}`}
+          data-testid="remove-star"
+          className={`remove-star ${isLiked ? "" : "filled"}`}
           onClick={() => handleDeleteClick(wordItem.id)}
         >
           <i className="fas fa-star"></i>
         </div>
         <div className="toggle-container">
-          <h2 className="title">
-            <strong>Word:</strong> {wordItem.word.word}
-          </h2>
-          <p className="my-word-list__text">{wordItem.word.phonetic}</p>
+          <h2 className="title">{wordItem.word.word}</h2>
         </div>
         <p
           className="see-more-btn"
-          onClick={() => setToggleInfo((prevInfo) => !prevInfo)}
+          onClick={() => {
+            setToggleInfo(!toggleInfo);
+          }}
         >
           See more
         </p>
       </div>
-
       <div className={`info-container ${toggleInfo ? "visible" : "hidden"}`}>
         {wordItem.word.meanings.map((meaning, index) => (
           <div key={index}>
             <Meaning meaning={meaning} />
           </div>
         ))}
-        <div>
-          {wordItem.word.phonetics.map((phonetic, index) => (
-            <div key={index}>
-              <Phonetic phonetics={phonetic} />
-            </div>
-          ))}
-        </div>
+
+        {wordItem.word.phonetics && wordItem.word.phonetics.length > 0 && (
+          <div className="show-phonetics">
+            {wordItem.word.phonetics.map((phonetic, index) => (
+              <div key={index}>
+                <Phonetic phonetics={phonetic} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
 }
+
 export default WordItem;
