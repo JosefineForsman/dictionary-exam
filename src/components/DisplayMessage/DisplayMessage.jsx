86d2ea538a1f1729e8a68368db9c49f88ea533b0
Josefine Forsman <josefineforsman1@gmail.com>
2023-09-24 19:29:22 +0200

    spelling corrector

--- src/components/DisplayMessage/DisplayMessage.jsx
+++ src/components/DisplayMessage/DisplayMessage.jsx
@@ -5,7 +5,7 @@ function DisplayMessage({ title, message, resolution }) {
   return (
     <div className="display-messages">
       <h2 className="display-messages__title">{title}</h2>
-      <p className="dislay-messages__message">{message}</p>
+      <p className="display-messages__message">{message}</p>
       <p className="display-messages__resolution">{resolution}</p>
     </div>
   );
